309cae61932b810b6a3be1fe3295045c3e71ad27
Starostin Sergey <Vlillen@yandex.ru>
2023-04-25 19:31:31 +0400

    Added Task33

--- Task33/main.py
+++ Task33/main.py
@@ -1,10 +1,20 @@
 '''
-Задача №37.
-Дано натуральное число N и последовательность из N элементов. Требуется вывести эту последовательность в обратном порядке.
+Задача №33.
+Хакер Василий получил доступ к классному журналу и хочет заменить все свои минимальные оценки на максимальные.
+Напишите программу, которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
+Input: 5 -> 1 3 3 3 4
+Output: 1 3 3 3 1
 '''
-number = int(input('Введите '))
-def return_list(number: int) -> None:
-    print(number)
-    if number > 0:
-        return_list(number - 1)
-return_list(number)
+import random
+
+list_score = []
+evaluations = int(input('Введите количество оценок: '))
+for i in range(evaluations):
+    list_score.append(random.randint(1, 5))
+print(f'Список оценок: {list_score}')
+max_point = max(list_score)
+min_point = min(list_score)
+for i in range(evaluations):
+    if list_score[i] == max_point:
+        list_score[i] = min_point
+print(f'Исправленные оценки: {list_score}')
